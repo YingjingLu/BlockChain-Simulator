ead3c5a36e6e4bf164e8074ed1c0bf76082bbeda
YingjingLu <yingjinglu2019us@gmail.com>
2020-10-18 20:02:11 -0400

    first commit

---
+++ src/main/java/com/blockchain/simulator/App.java
@@ -0,0 +1,13 @@
+package com.blockchain.simulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/blockchain/simulator/Message.java
@@ -0,0 +1,5 @@
+package com.blockchain.simulator;
+
+public class Message {
+
+}
