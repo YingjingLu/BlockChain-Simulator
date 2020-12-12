91eca324c2a26d27bc95db22b8927e4e9baf335b
YingjingLu <yingjinglu2019us@gmail.com>
2020-12-12 00:45:01 -0500

    Added message propagation and generalized round

--- src/test/java/com/blockchain/simulator/AppTest.java
+++ src/test/java/com/blockchain/simulator/AppTest.java
@@ -15,6 +15,8 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
+        String executionPath = System.getProperty("user.dir");
+        System.out.println(executionPath);
         assertTrue( true );
     }
 }
